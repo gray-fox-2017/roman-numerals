f88296525704f410eef13bbf8df04aa99d49801a
jamessraun <jeezybig@gmail.com>
2017-04-25 15:00:33 +0700

    roman-numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -4,18 +4,112 @@ function to_roman_old (num) {
 
 function to_roman (num) {
   // your implementation code here
+  var hasil=[];
+  for(var i=num; 0<i;)
+  {
+    //jika i lebih dari sama dengan 1000 maka push "M" ke hasil
+    if(i>=1000)
+    {
+
+      hasil.push("M");
+      i=i-1000;
+    }
+    //jika i lebih dari sama dengan 500 dan kurang dari 1000
+    else if(i>=500&&i<1000)
+    {
+      //jika i lebih dari sama dengan 900 dan kurang dari 1000 maka push "DM" ke hasil
+       if(i>=900&&i<1000)
+      {
+        hasil.push("DM");
+        i=i%900;
+      }
+      else
+        {
+          //jika yang lainnya push "D"
+          hasil.push("D");
+          i=i%500;
+        }
+
+    }
+    else if(i>=100&&i<500)
+    {
+      //jika i lebih dari sama dengan 399 dan kurang dari 500 maka push "CD" ke hasil
+      if(i>399&&i<500)
+      {
+        hasil.push("CD");
+        i=i%400;
+      }
+      else
+        {
+          hasil.push("C");
+          i=i%100;
+        }
+    }
+    else if(i>=50&&i<100)
+    {
+      if(i>89&&i<100)
+      {
+        hasil.push("CL");
+        i=i%90;
+      }
+      else
+        {
+          hasil.push("L");
+          i=i%50;
+        }
+    }
+    else if(i>=10&&i<50)
+    {
+       if(i>39&&i<50)
+      {
+        hasil.push("XL");
+        i=i%40;
+      }
+      else{hasil.push("X");
+      i=i%10;}
+    }
+    else if(i>=5&&i<10)
+    {
+      if(i===9)
+      {
+        hasil.push("IX");
+        i=i%9;
+      }
+      else
+      {
+        hasil.push("V");
+        i=i%5;
+      }
+
+    }
+    else if(i<5)
+    {
+       if(i===4)
+      {
+        hasil.push("IV");
+        i=i%4;
+      }
+      else
+      {
+       hasil.push("I");
+      i--;
+      }
+
+    }
+  }
+    return hasil.join("");
 }
 
 // Drive code
-console.log('My totally sweet testing script\n')
-console.log('input | expected | actual')
-console.log('———|—————|———')
-console.log('4     | IIII     | ', to_roman_old(4))
-console.log('9     | VIIII    | ', to_roman_old(9))
-console.log('13    | XIII     | ', to_roman_old(13))
-console.log('55    | LV       | ', to_roman_old(55))
-console.log('1453  | MCDLIII  | ', to_roman_old(1453))
-console.log('1646  | MDCXLVI  | ', to_roman_old(1646))
+// console.log('My totally sweet testing script\n')
+// console.log('input | expected | actual')
+// console.log('———|—————|———')
+// console.log('4     | IIII     | ', to_roman_old(4))
+// console.log('9     | VIIII    | ', to_roman_old(9))
+// console.log('13    | XIII     | ', to_roman_old(13))
+// console.log('55    | LV       | ', to_roman_old(55))
+// console.log('1453  | MCDLIII  | ', to_roman_old(1453))
+// console.log('1646  | MDCXLVI  | ', to_roman_old(1646))
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
