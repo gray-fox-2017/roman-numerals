fa37b8bc7b74a11b0c63edf35a593ba038ac6348
AndrewSenewe <sindrew.christ@gmail.com>
2017-04-25 16:57:39 +0800

    Roman Number Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -4,18 +4,30 @@ function to_roman_old (num) {
 
 function to_roman (num) {
   // your implementation code here
+var code = [["","I","II","III","IV","V","VI","VII","VIII","IX"],
+            ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+            ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+            ["","M","MM","MMM"]];
+var ans = '';
+var numStr = num.toString().split('').reverse();
+
+for (var i = 0; i<numStr.length; i++) {
+  ans = code[i][Number(numStr[i])] + ans;
+}
+return ans;
 }
 
+
 // Drive code
-console.log('My totally sweet testing script\n')
-console.log('input | expected | actual')
-console.log('———|—————|———')
-console.log('4     | IIII     | ', to_roman_old(4))
-console.log('9     | VIIII    | ', to_roman_old(9))
-console.log('13    | XIII     | ', to_roman_old(13))
-console.log('55    | LV       | ', to_roman_old(55))
-console.log('1453  | MCDLIII  | ', to_roman_old(1453))
-console.log('1646  | MDCXLVI  | ', to_roman_old(1646))
+// console.log('My totally sweet testing script\n')
+// console.log('input | expected | actual')
+// console.log('———|—————|———')
+// console.log('4     | IIII     | ', to_roman_old(4))
+// console.log('9     | VIIII    | ', to_roman_old(9))
+// console.log('13    | XIII     | ', to_roman_old(13))
+// console.log('55    | LV       | ', to_roman_old(55))
+// console.log('1453  | MCDLIII  | ', to_roman_old(1453))
+// console.log('1646  | MDCXLVI  | ', to_roman_old(1646))
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
