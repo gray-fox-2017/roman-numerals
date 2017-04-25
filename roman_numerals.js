a88e886f7a63fd367896d21ad90a48b9705ee1cf
Parel Wellman Hutahaean <parel.hutahaean@gmail.com>
2017-04-25 18:21:45 +0700

    Roman Numerals Function with looping done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,21 +1,21 @@
-function to_roman_old (num) {
-  // your implementation code here
-}
+// Priambodo Nur Kurniawan | Roman Numerals
 
 function to_roman (num) {
-  // your implementation code here
+  var romanCodes = [
+    ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'], // satuan
+    ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'], // puluhan
+    ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'], // ratusan
+    ['', 'M', 'MM', 'MMM'] // ribuan
+  ]
+  var roman = num.toString().split('').reverse(); // mengubah number menjadi string, memecah number & membaliknya.
+  var str = '';
+  for (var i=0; i<roman.length; i++ ){
+    str = romanCodes[i][parseInt(roman[i])] + str;
+  }
+  return str;
 }
-
+console.log(to_roman(476));
 // Drive code
-console.log('My totally sweet testing script\n')
-console.log('input | expected | actual')
-console.log('———|—————|———')
-console.log('4     | IIII     | ', to_roman_old(4))
-console.log('9     | VIIII    | ', to_roman_old(9))
-console.log('13    | XIII     | ', to_roman_old(13))
-console.log('55    | LV       | ', to_roman_old(55))
-console.log('1453  | MCDLIII  | ', to_roman_old(1453))
-console.log('1646  | MDCXLVI  | ', to_roman_old(1646))
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
@@ -27,6 +27,5 @@ console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
 
 module.exports = {
-  to_roman_old,
   to_roman
 }
