6b24926cc4d79b2b39462f0076ed04edd5eb11e5
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-25 11:28:59 +0700

    no bug detected

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,11 +1,29 @@
 function to_roman_old (num) {
-  // your implementation code here
-}
+     /* http://www.selftaughtjs.com/algorithm-sundays-converting-roman-numerals/ */
+     var result = '';
+     var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 5, 1];
+     var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL" , "X", "V", "I"];
+     for (i = 0 ; i < decimal.length ; i++) {
+       while (num%decimal[i] < num) {
+         result = result + roman[i];
+         num = num - decimal[i];
+       }
+     }
+     return result;
+     }
 
 function to_roman (num) {
-  // your implementation code here
-}
-
+      var result = '';
+      var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+      var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+      for (var i = 0; i < decimal.length; i++) {
+        while (num%decimal[i] < num) {
+           result = result + roman[i];
+           num = num - decimal[i];
+        }
+      }
+      return result;
+      }
 // Drive code
 console.log('My totally sweet testing script\n')
 console.log('input | expected | actual')
