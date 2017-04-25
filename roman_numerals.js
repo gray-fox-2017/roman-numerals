bc7df66875c6004e627ca79c669d833c1a729fe9
Hacktiv8 <hacktiv8-11@Hacktiv8-11.local>
2017-04-25 15:11:03 +0700

    penambahan banyak kode pada roman numeral, tapi abis ini mau d rombak

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,9 +1,148 @@
 function to_roman_old (num) {
-  // your implementation code here
+  // yourimplementation code here
+
 }
 
 function to_roman (num) {
-  // your implementation code here
+   //your implementation code here
+   if (num > 1000) {
+     var ribuan = Math.sqrt(num/1000);
+   } else {
+     var ribuan = num;
+   }
+
+   if (num > 1000) {
+     var sisaRibuan = num % 1000;
+   } else {
+     var sisaRibuan = num
+   }
+
+   if (num > 500) {
+     var limaRatusan = Math.sqrt(sisaRibuan/500);
+   } else {
+     var limaRatusan = num;
+   }
+
+   if (num > 500) {
+     var sisaLimaRatusan = sisaRibuan%500;
+   } else {
+     var sisaLimaRatusan = num;
+   }
+
+   if (num > 100) {
+     var ratusan = Math.sqrt(sisaLimaRatusan/100);
+   } else {
+     var ratusan = num;
+   }
+
+   if (num > 100) {
+     var sisaRatusan = sisaLimaRatusan%100;;
+   } else {
+     var sisaRatusan = num;
+   }
+
+   if (num > 50) {
+     var limaPuluhan = Math.sqrt(sisaRatusan/50);
+   } else {
+     var limaPuluhan = num;
+   }
+
+   if (num > 50) {
+     var sisaLimaPuluhan = sisaRatusan%50;
+   } else {
+     var sisaLimaPuluhan = num;
+   }
+
+   if (num > 10) {
+     var puluhan = Math.sqrt(sisaLimaPuluhan/10);
+   } else {
+     var puluhan = num;
+   }
+
+   if (num > 10) {
+     var sisaPuluhan = sisaLimaPuluhan%10;
+   } else {
+     var sisaPuluhan = num;
+   }
+
+   if (num>5) {
+     var limaan = Math.sqrt(sisaPuluhan/5);
+   } else {
+     var limaan = num
+   }
+
+   if (num>5) {
+     var sisaLimaan = sisaPuluhan%5;
+   } else {
+     var sisaLimaan = num;
+   }
+
+   var satuan = sisaLimaan;
+
+var hasil = [];
+
+if (num>1000) {
+  for (let i=0;i<ribuan;i++) {
+  hasil.push('M')
+  }
+  for (let i=0;i<limaRatusan;i++) {
+  hasil.push('D')
+  }
+  for (let i=0;i<ratusan;i++) {
+    hasil.push('C')
+  }
+  for (let i=0;i<limaPuluhan;i++) {
+  hasil.push('L')
+  }
+  for (let i=0;i<puluhan;i++) {
+  hasil.push('X')
+  }
+  for (let i=0;i<limaan;i++) {
+  hasil.push('V')
+  }
+  if (num>1) {
+    for (let i=0;i<satuan;i++) {
+    hasil.push('I')
+    }
+  }
+  //untuk limaratusan
+} else if (num>500) {
+  for (let i=0;i<limaRatusan;i++) {
+  hasil.push('D')
+  }
+}
+
+if (num>100) {
+  for (let i=0;i<ratusan;i++) {
+    hasil.push('C')
+  }
+}
+
+if (num>50){
+  for (let i=0;i<limaPuluhan;i++) {
+  hasil.push('L')
+  }
+}
+
+if (num>10){
+  for (let i=0;i<puluhan;i++) {
+  hasil.push('X')
+  }
+}
+
+if (num>5){
+  for (let i=0;i<limaan;i++) {
+  hasil.push('V')
+  }
+}
+
+if (num>1) {
+  for (let i=0;i<satuan;i++) {
+  hasil.push('I')
+  }
+}
+
+return hasil.join('');
 }
 
 // Drive code
