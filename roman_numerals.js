b6612a55ab27d9b53445ca6d09fe8ef2b8608003
Antoni Angga <antoniangga14@gmail.com>
2017-04-25 18:08:25 +0700

    Roman Number Done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,21 +1,32 @@
-function to_roman_old (num) {
-  // your implementation code here
-}
-
 function to_roman (num) {
   // your implementation code here
-}
+    var TulisanArray = "";
+    var ArrNumber = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    var ArrRoman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
 
-// Drive code
-console.log('My totally sweet testing script\n')
-console.log('input | expected | actual')
-console.log('———|—————|———')
-console.log('4     | IIII     | ', to_roman_old(4))
-console.log('9     | VIIII    | ', to_roman_old(9))
-console.log('13    | XIII     | ', to_roman_old(13))
-console.log('55    | LV       | ', to_roman_old(55))
-console.log('1453  | MCDLIII  | ', to_roman_old(1453))
-console.log('1646  | MDCXLVI  | ', to_roman_old(1646))
+    for(var i = 0; i < ArrNumber.length; i++) {
+        //check angka nya ada di Arr Roman Tersebut dengan mengecheck dengan angka tersebut masuk dimana
+        if (num >= ArrNumber[i]) {
+            //jika angka yang true lakukan pengecekan di bawah dari 5 sampai di bawah dari 8
+            if (5 <= num && num <= 8) {
+                // lakukan num - 5 untuk selain Romawi I;
+                num -= 5;
+            }
+            // jika angka yang true di bawah dari angka 1 sampai angka ke 3;
+            else if (1 <= num && num <= 3) {
+                // lakukan num - 1 untuk Romawi I;
+                num -= 1;
+            }
+            else {
+                // lakukan pemotongan number;
+                num -= ArrNumber[i];
+            }
+            TulisanArray += ArrRoman[i];
+            i--;
+        }
+    }
+    return TulisanArray;
+}
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
@@ -27,6 +38,5 @@ console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
 
 module.exports = {
-  to_roman_old,
   to_roman
 }
