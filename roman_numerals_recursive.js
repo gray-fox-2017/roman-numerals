150d95a9745481012f58ae4b86f517a878dfb977
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-04-25 20:55:17 +0700

    Second Task Challenge Roman Numerals Recursive

---
+++ roman_numerals_recursive.js
@@ -0,0 +1,43 @@
+function to_roman (num) {
+  // your implementation code here
+var romanArr = [
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I']
+];
+  if (num === 0) {
+    return '';
+  }
+  for (var i = 0; i < romanArr.length; i++) {
+    if (num >= romanArr[i][0]) {
+      return romanArr[i][1] + to_roman(num - romanArr[i][0]);
+    }
+  }
+	
+}
+
+
+
+console.log('My totally sweet testing script for new roman\n')
+console.log('input | expected | actual')
+console.log('———|—————|———')
+console.log('4     | IV       | ', to_roman(4))
+console.log('9     | IX       | ', to_roman(9))
+console.log('13    | XIII     | ', to_roman(13))
+console.log('1453  | MCDLIII  | ', to_roman(1453))
+console.log('1646  | MDCXLVI  | ', to_roman(1646))
+
+module.exports = {
+  
+  to_roman
+}
