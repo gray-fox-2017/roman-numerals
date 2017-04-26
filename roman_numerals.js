d1bd33e1ec7c842a7cb400cadbc4db13877ae6f0
hakiemaul <noersedya@gmail.com>
2017-04-26 15:09:06 +0700

    redo all code to fix the error

--- roman_numerals.js
+++ roman_numerals.js
@@ -7,11 +7,12 @@ function to_roman (num) {
   let rom=[];
   if(num>=1000) {
     let jumlahSimbol=Math.floor(num/1000);
+    let n=jumlahSimbol;
     while(jumlahSimbol>0) {
       rom.push('M');
       jumlahSimbol--;
     }
-    num=num%1000;
+    num=num%(n*1000);
   }
   if(num>=500) {
     let jumlahSimbol=Math.floor(num/500);
@@ -20,13 +21,19 @@ function to_roman (num) {
       jumlahSimbol--;
     }
     num=num%500;
+    if(num>=400) {
+      rom.pop()
+      rom.push('C');
+      rom.push('M');
+      num=(num+500)%100;
+    }
   }
   if(num>=100) {
-    let jumlahSimbol=Math.floor(num/100);
+    let hasil=Math.floor(num/100);
     let hitung=0;
-    while(jumlahSimbol>0) {
+    while(hasil>0) {
       rom.push('C');
-      jumlahSimbol--;
+      hasil--;
       hitung++;
       if(hitung==4) {
         while(hitung>1) {
@@ -45,13 +52,19 @@ function to_roman (num) {
       jumlahSimbol--;
     }
     num=num%50;
+    if(num>=40) {
+      rom.pop()
+      rom.push('X');
+      rom.push('L');
+      num=(num+50)%10;
+    }
   }
   if(num>=10) {
-    let jumlahSimbol=Math.floor(num/10);
+    let hasil=Math.floor(num/10);
     let hitung=0;
-    while(jumlahSimbol>0) {
+    while(hasil>0) {
       rom.push('X');
-      jumlahSimbol--;
+      hasil--;
       hitung++;
       if(hitung==4) {
         while(hitung>1) {
@@ -71,10 +84,10 @@ function to_roman (num) {
     }
     num=num%5;
     if(num>=4) {
-      rom.pop();
+      rom.pop()
       rom.push('I');
       rom.push('X');
-      num=0;
+      num=(num+5)%1;
     }
   }
   if(num>=1) {
@@ -115,7 +128,7 @@ console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('1646  | MDCXLVI  | ', to_roman(50))
 
 module.exports = {
   to_roman_old,
