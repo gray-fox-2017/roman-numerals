c14ceeb674b3d73b73dd289210554ef16b9314c6
fajarkarim <fajarabdulkarim@gmail.com>
2017-04-27 11:20:41 +0700

    simplified the condition

--- roman_numerals.js
+++ roman_numerals.js
@@ -8,51 +8,45 @@ function to_roman (num) {
   let count = 0;
 
   while (num > 0) {
-    if (num - 1000 >= 0 && num >= 1000) {
-
+    if (num >= 1000) {
       num -= 1000;
       romanChar += "M";
-    } else if (num - 500 >= 0 && num >= 500) {
-
-        num -= 500;
-        romanChar += "D"
-    } else if (num - 100 >= 0 && num >= 100) {
-      if (num - 400 > 0 && num >= 400) {
-        num -= 400;
-        romanChar += "CD";
-      } else {
-        num -= 100;
-        romanChar += "C";
-      }
-    } else if (num - 50 >= 0 && num >= 50) {
+    } else if (num >= 900) {
+      num -= 900;
+      romanChar += "CM";
+    } else if (num >= 500) {
+      num -= 500;
+      romanChar += "D"
+    } else if (num >= 400) {
+      num -= 400;
+      romanChar += "CD";
+    } else if (num >= 100) {
+      num -= 100;
+      romanChar += "C";
+    } else if (num >= 90) {
+      num -= 90;
+      romanChar += "CX";
+    } else if (num >= 50) {
       num -= 50;
       romanChar += "L";
-    } else if (num - 10 >= 0 && num >= 10) {
-      if (num - 40 >= 0 && num >= 40) {
-        num -= 40;
-        romanChar += "XL";
-      } else {
-        num -= 10;
-        romanChar += "X";
-      }
-
-    } else if (num - 5 >= 0 && num >= 5) {
-      if (num - 9 >= 0 && num >= 9) {
-        num -= 9;
-        romanChar += "IX";
-      } else {
-        num -= 5;
-        romanChar += "V";
-      }
-
-    } else if (num - 1 >= 0 && num >= 1) {
-      if (num - 4 >= 0 && num >= 4) {
-        num -= 4;
-        romanChar += "IV";
-      } else {
-        num -= 1;
-        romanChar += "I";
-      }
+    } if (num >= 40) {
+      num -= 40;
+      romanChar += "XL";
+    } else if (num >= 10) {
+      num -= 10;
+      romanChar += "X";
+    } else if (num >= 9) {
+      num -= 9;
+      romanChar += "IX";
+    } else if (num >= 5) {
+      num -= 5;
+      romanChar += "V";
+    } else if (num >= 4) {
+      num -= 4;
+      romanChar += "IV";
+    } else if (num >= 1) {
+      num -= 1;
+      romanChar += "I";
     }
   }
   return romanChar;
@@ -76,7 +70,7 @@ console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('1646  | MDCXLVI  | ', to_roman(2900))
 
 module.exports = {
   to_roman_old,
